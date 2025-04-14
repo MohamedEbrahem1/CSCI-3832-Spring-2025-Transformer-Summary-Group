eb64c4ce16c6fb46e4d2bbb001df2e98614be1df
Miles Zheng <mzheng.012@gmail.com>
2025-04-14 09:36:58 -0600

    test comment 2

--- Simple_RNN.py
+++ Simple_RNN.py
@@ -141,3 +141,4 @@ def train_lstm_classification(
 # model = LSTMModel(embedding_matrix, lstm_hidden_size=50, num_lstm_layers=2, bidirectional=True)
 # model, best_model = train_lstm_classification(model, train_dataset, valid_dataset, batch_size=128, epochs=1)
 # comment
+# comment 2
